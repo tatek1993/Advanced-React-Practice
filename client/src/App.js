6ec1e0762032380e5249fc3a205f2b1c55949367
Tate Keller <tatek1993@gmail.com>
2020-02-13 18:20:54 -0600

    Refactor App.js into a class component
    *Refactored App.js into a class component and set state

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  
-    return (
+// C C R 
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  object = {
+    practice: {
+      number: 1
+    }
+  };
+
+  console.log(Array.isArray(this.object.practice));
+
+  componentDidMount(){
+    axios.get(`http://localhost:5000/api/footballers`)
+    .then(res => {
+      // console.log(res.data);
+      // console.table(res.data);
+      this.setState({players: res.data})
+      console.log(Array.isArray(this.state.players));
+    })
+    .catch()
+  }
+    render(){
+     return (
     <div className="App">
       <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
     </div>
-  );
+    ); 
+  }
+    
 }
 
 export default App;
